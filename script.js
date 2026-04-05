3d310caaac7db3d3753fb40a12302bb8239aa237
TheGuyThatMakes <273538533+TheGuyThatMakes@users.noreply.github.com>
2026-04-05 10:48:52 +0800

    Added dynamism

---
+++ script.js
@@ -0,0 +1,15 @@
+let projects=["TrixxatiNum.js"]
+let empty=[]
+ul = document.querySelector("#ul")
+if (projects!==empty){
+    ul.innerHTML=""
+    projects.forEach(element => {
+        let node = document.createElement("li");
+        let subnode = document.createElement("a");
+        subnode.href = "https://theguythatmakes.github.io/"+element
+        let textnode = document.createTextNode(element)
+        subnode.appendChild(textnode)
+        node.appendChild(subnode)
+        ul.appendChild(node)
+    });
+}
